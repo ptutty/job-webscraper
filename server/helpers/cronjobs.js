8b1a72130a142f7180d053c216b06a4f24ba5a9b
Philip Tutty <p.tutty@warwick.ac.uk>
2016-05-16 13:49:06 +0100

    cron test

--- server/helpers/cronjobs.js
+++ server/helpers/cronjobs.js
@@ -7,14 +7,35 @@ var JobsImportCtrl = require('../controllers/jobsImportCtrl'); // shortlist cont
 var CronJob = require('cron').CronJob;
 
 
-new CronJob('00 40 8 * * 1-7', function() {
-    /*
-     * Runs 7 days a week
-     * at 12:30:00 PM.
-     */
-    console.log("running the remove old jobs cron job");
-    JobsTidyCtrl.checkDeadlineCron();
-    console.log("running the import cron jobs");
-    JobsImportCtrl.importJobs();
 
-}, null, true, 'America/New_York');
+
+var job = new CronJob('00 52 8 * * 0-6', function() {
+        /*
+         * Runs every weekday (Monday through Friday)
+         * at 11:30:00 AM. It does not run on Saturday
+         * or Sunday.
+         */
+
+        console.log("running the remove old jobs cron job");
+        JobsTidyCtrl.checkDeadlineCron();
+        console.log("running the import cron jobs");
+        JobsImportCtrl.importJobs();
+
+    }, function () {
+        console.log("cron job has run");
+    },
+    true, /* Start the job right now */
+    'America/New_York' /* Time zone of this job. */
+);
+
+
+
+//
+// new CronJob('00 40 8 * * 0-6', function() {
+//     /*
+//      * Runs 7 days a week
+//      * at 12:30:00 PM.
+//      */
+//
+//
+// }, null, true, 'America/New_York');
