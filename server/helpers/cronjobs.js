abda9c69bcef7e960da668be08a9ade095938d86
Philip Tutty <p.tutty@warwick.ac.uk>
2016-05-23 10:17:07 +0100

    cron patch

--- server/helpers/cronjobs.js
+++ server/helpers/cronjobs.js
@@ -9,29 +9,30 @@ var CronJob = require('cron').CronJob;
 
 
 
-// var job = new CronJob('00 35 12 * * 0-6', function() {
-//         /*
-//          * Runs everyday
-//          * at 8:55 EST or approx 1:55pm UK time - server is in NYC
-//          */
-//
-//         JobsTidyCtrl.checkDeadlineCron();
-//         JobsImportCtrl.importJobs();
-//
-//     }, function () {
-//         console.log("cron job has run");
-//     },
-//     true, /* Start the job right now */
-//     'America/New_York' /* Time zone of this job. */
-// );
+var job = new CronJob('00 20 5 * * 0-6', function() {
+        /*
+         * Runs everyday
+         * at 8:55 EST or approx 1:55pm UK time - server is in NYC
+         */
+        console.log("start cron job");
+        JobsTidyCtrl.checkDeadlineCron();
+        JobsImportCtrl.importJobs();
+
+    }, function () {
+        console.log("cron job has run");
+    },
+    true, /* Start the job right now */
+    'America/New_York' /* Time zone of this job. */
+);
 
 
 
+/* cron test */
 
-var CronJob = require('cron').CronJob;
-new CronJob('* * * * * *', function() {
-    console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+// var CronJob = require('cron').CronJob;
+// new CronJob('* * * * * *', function() {
+//     console.log('You will see this message every second');
+// }, null, true, 'America/Los_Angeles');
 
 
 
