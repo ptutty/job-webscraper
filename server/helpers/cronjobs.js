04eaaeb4ccdbb5eef3d0a9a52e074e0624cf6861
Philip Tutty <p.tutty@warwick.ac.uk>
2016-05-24 13:25:35 +0100

    another cron test

--- server/helpers/cronjobs.js
+++ server/helpers/cronjobs.js
@@ -4,35 +4,34 @@
 var JobsTidyCtrl = require('../controllers/jobsTidyCtrl'); // tidy up old jobs
 var JobsImportCtrl = require('../controllers/jobsImportCtrl'); // shortlist controller
 
-var CronJob = require('cron').CronJob;
 
 
+var cron = require('cron');
+
+// running job every 1 minutes:
+var cronJob = cron.job("0 */1 * * * *", function(){
+    console.info('cron job completed' + date );
+});
+cronJob.start();
 
 
-var job = new CronJob('00 20 5 * * 0-6', function() {
-        /*
-         * Runs everyday
-         * at 8:55 EST or approx 1:55pm UK time - server is in NYC
-         */
-        console.log("start cron job");
-        JobsTidyCtrl.checkDeadlineCron();
-        JobsImportCtrl.importJobs();
-
-    }, function () {
-        console.log("cron job has run");
-    },
-    true, /* Start the job right now */
-    'America/New_York' /* Time zone of this job. */
-);
-
-
-
-/* cron test */
 
 // var CronJob = require('cron').CronJob;
-// new CronJob('* * * * * *', function() {
-//     console.log('You will see this message every second');
-// }, null, true, 'America/Los_Angeles');
+// var job = new CronJob('00 20 5 * * 0-6', function() {
+//         /*
+//          * Runs everyday
+//          * at 5:20 EST or approx 10:20am UK time - server is in NYC
+//          */
+//         console.log("start cron job");
+//         JobsTidyCtrl.checkDeadlineCron();
+//         JobsImportCtrl.importJobs();
+//
+//     }, function () {
+//         console.log("cron job has run");
+//     },
+//     false, /* Start the job right now */
+//     'America/New_York' /* Time zone of this job. */
+// );
 
 
 
