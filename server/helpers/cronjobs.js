3fc7d37d18f424c57f3134a79a300f81eb142a6e
philip tutty <ptutty@gmail.com>
2016-05-31 20:55:26 +0100

    cron test

--- server/helpers/cronjobs.js
+++ server/helpers/cronjobs.js
@@ -6,28 +6,28 @@ var JobsImportCtrl = require('../controllers/jobsImportCtrl'); // shortlist cont
 
 
 
-// var cron = require('cron');
-//
-// // running job twice a day
-// var cronJob = cron.job("00 30 5 * * 1-6", function(){
-//         console.log("start cron job");
-//         JobsTidyCtrl.checkDeadlineCron();
-//         JobsImportCtrl.importJobs();
-// }, function () {
-//     console.log("cron job has run");
-//    },
-//     true,
-//     'America/New_York' /* Time zone of this job. */
-// );
-// cronJob.start();
-
-
-
 var cron = require('cron');
-var cronJob = cron.job("0 */1 * * * *", function(){
-    // perform operation e.g. GET request http.get() etc.
-    console.info('cron job completed');
-});
+
+// running job twice a day
+var cronJob = cron.job("00 55 15 * * 1-5", function(){
+        console.log("start cron job");
+        JobsTidyCtrl.checkDeadlineCron();
+        JobsImportCtrl.importJobs();
+}, function () {
+    console.log("cron job has run");
+   },
+    true,
+    'America/New_York' /* Time zone of this job. */
+);
 cronJob.start();
+
+
+
+// var cron = require('cron');
+// var cronJob = cron.job("0 */1 * * * *", function(){
+//     // perform operation e.g. GET request http.get() etc.
+//     console.info('cron job completed');
+// });
+// cronJob.start();
 
 
